c24a575622b50f9260fb91a7b755dcb42f34b6e2
Ashutosh Jindal <Ash.Jindal@ext.ons.gov.uk>
2019-08-28 15:43:39 +0100

    --amend

--- flow_logging.tf
+++ flow_logging.tf
@@ -1,25 +1,25 @@
 #Set up flow logging for cloudwatch
 
 resource "aws_flow_log" "results_flow_log" {
-  iam_role_arn    = "${aws_iam_role.results_flow_log_iam_role.arn}"
-  log_destination = "${aws_cloudwatch_log_group.results_flow_log_group.arn}"
+  iam_role_arn    = aws_iam_role.results_flow_log_iam_role.arn
+  log_destination = aws_cloudwatch_log_group.results_flow_log_group.arn
   traffic_type    = "ALL"
-  vpc_id          = "${aws_vpc.main_vpc.id}"
+  vpc_id          = aws_vpc.main_vpc.id
 }
 
 resource "aws_cloudwatch_log_group" "results_flow_log_group" {
-  name = "results_flow_log_group"
+  name = "results_flow_log_group2"
 }
 
 resource "aws_iam_role" "results_flow_log_iam_role" {
   name = "results_flow_log_iam_role"
 
-  assume_role_policy = "${file("${path.module}/flow_logging_tr_policy.json")}"
+  assume_role_policy = file("${path.module}/flow_logging_tr_policy.json")
 }
 
 resource "aws_iam_role_policy" "cloudwatch_access_policy" {
   name = "cloudwatch_access_policy"
-  role = "${aws_iam_role.results_flow_log_iam_role.id}"
+  role = aws_iam_role.results_flow_log_iam_role.id
 
   policy = <<EOF
 {
